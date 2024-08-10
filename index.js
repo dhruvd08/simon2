0c396d847136ca28c06f8938231b083f99b71d5f
Maulik Dave <mauldave@gmail.com>
2024-08-10 15:47:01 +0530

    correct index.html path

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/home", (req, res) => {
