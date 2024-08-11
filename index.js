76e93c92d832f2eb6a1a9d9e8868dbb2e81563ae
Maulik Dave <mauldave@gmail.com>
2024-08-11 22:51:56 +0530

    change sort range rows to 2000

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ async function _sortSheet(googleSheetClient) {
           range: {
             sheetId: 0,
             startRowIndex: 1,
-            endRowIndex: 500,
+            endRowIndex: 2000,
             startColumnIndex: 0,
             endColumnIndex: 3,
           },
