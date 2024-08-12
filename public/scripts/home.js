242f4956d73b8b4dee0a82d2da751b4f53d78d1e
Maulik Dave <mauldave@gmail.com>
2024-08-12 09:44:40 +0530

    add player name input validations

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,14 +1,27 @@
 const playBtn = document.querySelector(".btn-play");
 const alertMsg = document.querySelector(".alert-danger");
 
+function allLetter(inputtxt) {
+  let regex = /^[A-Za-z]+$/;
+  if (regex.test(inputtxt)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 playBtn.addEventListener("click", () => {
-  const playerName = document.querySelector(".name-input").value;
+  const playerName = document.querySelector(".name-input").value.trim();
+
   if (playerName === "") {
     alertMsg.textContent = "Player name cannot be blank.";
     alertMsg.removeAttribute("hidden");
-  } else {
+  }else if (!allLetter(playerName)) {
+    alertMsg.textContent = "Player name can only have alphabets.";
+    alertMsg.removeAttribute("hidden");}
+  else {
     alertMsg.setAttribute("hidden", true);
-    localStorage.setItem("currentPlayer", playerName.trim());
+    localStorage.setItem("currentPlayer", playerName);
     window.open("/game", "_self");
     //registerPlayer(playerName);
   }
