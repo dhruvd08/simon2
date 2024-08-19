d83507b8266cb4a742a0adf944fb7cc4879ac6c8
Maulik Dave <mauldave@gmail.com>
2024-08-19 19:56:33 +0530

    move to postgres

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from "express";
-import { google } from "googleapis";
 import path from "path";
+import pg from "pg";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-const sheetId = "1Bclbev44BoVoBnjq3VRsj7xSiVZNz1glAYr8NA2hoTg";
-const tabName = "score";
-const range = "A:C";
-const serviceAccountKeyFile = path.join(process.cwd(), "/simon2-432115-37b7ee736007.json");
-const scope = "https://www.googleapis.com/auth/spreadsheets";
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+db.connect();
+
 
 app.use(express.static(path.join(process.cwd() + "/public/")));
 app.use(express.json());
@@ -30,11 +35,12 @@ app.get("/game", (req, res) => {
 
 app.get("/leaderboard", async (req, res) => {
   let scores = [];
-  let rankedScores = [];
-  const googleSheetClient = await _getGoogleSheetClient();
-  scores = await _getScores(googleSheetClient, sheetId, tabName, range);
+
+  const result = await db.query("SELECT name, score, upload_time FROM score ORDER BY score DESC, upload_time ASC LIMIT 10");
+  scores = result.rows;
+
   for (let score of scores) {
-    score[2] = new Date(score[2]).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"})
+    score.upload_time = new Date(score.upload_time).toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"})
   }
   //console.log(rankedScores);
   res.render("leaderboard.ejs", {
@@ -46,12 +52,7 @@ app.get("/leaderboard", async (req, res) => {
 
 app.post("/score", async (req, res) => {
   const score = req.body;
-  const data = [[score.name, score.score, score.uploadTime]];
-  const googleSheetClient = await _getGoogleSheetClient();
-  await _addScore(googleSheetClient, sheetId, tabName, range, data);
-
-  const response = await _sortSheet(googleSheetClient);
-
+  const result = await db.query("INSERT INTO score (name, score, upload_time) VALUES ($1, $2, $3)", [score.name, score.score, score.uploadTime]);
   res.send({ statusCode: 200 });
 });
 
@@ -59,69 +60,3 @@ app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-async function _getGoogleSheetClient() {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: serviceAccountKeyFile,
-    scopes: [scope],
-  });
-  const authClient = await auth.getClient();
-  return google.sheets({
-    version: "v4",
-    auth: authClient,
-  });
-}
-
-async function _sortSheet(googleSheetClient) {
-  const resource = {
-    requests: [
-      {
-        sortRange: {
-          range: {
-            sheetId: 0,
-            startRowIndex: 1,
-            endRowIndex: 3000,
-            startColumnIndex: 0,
-            endColumnIndex: 3,
-          },
-
-          sortSpecs: [
-            {
-              dimensionIndex: 1,
-              sortOrder: "DESCENDING",
-            },
-          ],
-        },
-      },
-    ],
-    //responseRanges: ["A2:C11"],
-  };
-
-  const response = await googleSheetClient.spreadsheets.batchUpdate({
-    spreadsheetId: sheetId,
-    resource: resource,
-  });
-}
-
-async function _getScores(googleSheetClient, sheetId, tabName, range) {
-  const res = await googleSheetClient.spreadsheets.values.get({
-    spreadsheetId: sheetId,
-    range: `${tabName}!A1:C11`,
-  });
-
-  const scores = res.data.values;
-  scores.splice(0, 1);
-  return scores;
-}
-
-async function _addScore(googleSheetClient, sheetId, tabName, range, data) {
-  await googleSheetClient.spreadsheets.values.append({
-    spreadsheetId: sheetId,
-    range: `${tabName}!${range}`,
-    valueInputOption: "USER_ENTERED",
-    insertDataOption: "INSERT_ROWS",
-    resource: {
-      majorDimension: "ROWS",
-      values: data,
-    },
-  });
-}
